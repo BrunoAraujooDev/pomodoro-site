114afab57e08d2a01c01e6eac7f8ebda6886848b
Bruno Araujo <bruno.aoliveira@al.infnet.edu.br>
2022-08-30 19:13:53 -0300

    finalizando layout da tabela e do form

--- src/pages/History.tsx
+++
@@ -1,5 +0,0 @@
-export function History() {
-  return <h2>olá 2</h2>
-}
-
-export default History
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 
 import { Play } from 'phosphor-react'
-import { CountDownContainer, FormContainer, HomeContainer, Separator } from './styles'
+import { CountDownContainer, FormContainer, HomeContainer, MinusAmountInput, Separator, StartCountdownButton, TaskInput } from './styles'
 
 
 export function Home() {
@@ -10,10 +10,14 @@ export function Home() {
       <form action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <input type="text" id="task" />
+          <TaskInput type="text" id="task" list="task-suggestion"  placeholder='Dê um nome para o seu projeto'/>
+          
+          <datalist id="task-suggestion">
+            <option value="Projeto 1"></option>
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <input type="number" id="minutesAmount" />
+          <MinusAmountInput type="number" id="minutesAmount" placeholder='00' step={5} min={5} max={60}/>
 
           <span>minutos.</span>
         </FormContainer>
@@ -28,10 +32,10 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <button type="submit">
+        <StartCountdownButton type="submit" disabled>
           <Play/>
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
